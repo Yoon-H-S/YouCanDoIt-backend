9ae66ebea71bae2025d9b0e9f406bb7151c9e06a
YHS <dlddu7064@naver.com>
2023-09-26 23:01:54 +0900

    스티커 생성 구현
    cron  서비스 한 클래스에 정리

--- src/main/java/com/example/youcandoit/repository/StickerRepository.java
+++ src/main/java/com/example/youcandoit/repository/StickerRepository.java
@@ -1,5 +1,6 @@
 package com.example.youcandoit.repository;
 
+import com.example.youcandoit.dto.DiyCountDto;
 import com.example.youcandoit.entity.Id.StickerId;
 import com.example.youcandoit.entity.MemberEntity;
 import com.example.youcandoit.entity.StickerEntity;
@@ -13,42 +14,26 @@ import java.awt.*;
 import java.sql.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public interface StickerRepository extends JpaRepository<StickerEntity, StickerId> {
-//    /** 스티커 데이터 삽입*/
-//    @Modifying
-//    @Transactional
-//    @Query("insert into StickerEntity(stickerDate, memId, totalCount, successCount)"+
-//            "select :date, p.memId, p.count(*), :count from GroupPersonEntity p" +
-//            "join GroupEntity g on p.groupNumber = g.groupNumber" +
-//            "where g.groupClass = 2 and g.groupState = 2 and p.personStatus = 2")
-//    void insertSticker(@Param("date")Date date, @Param("count")Integer successCount);
 
-//    @Modifying
-//    @Transactional
-//    @Query("insert into StickerEntity(stickerDate, memId, totalCount, successCount)"+
-//            "select :date, p.memId, p.count(*), :count from GroupPersonEntity p" +
-//            "join g on p.groupNumber = g.groupNumber" +
-//            "where g.groupClass = 2 and g.groupState = 2 and p.personStatus in (1, 2)")
-//    void insertSticker(@Param("date")Date date, @Param("count")Integer successCount);
-
-//    @Query("select count(*) from GroupPersonEntity gp " +
-//            "join GroupEntity g on gp.groupNumber = g.groupNumber " +
-//            "where gp.memId = :loginId and groupState = 2 and groupClass = 2")
-//    int challengeTotalCount(@Param("loginId")String loginId);
-
-    /** diy 조회된 애들 총 카운트 */
-    @Query(value = "select g.memId, count(*) from GroupPersonEntity p " +
-            "join ChallengeGroupEntity g on p.groupNumber = g.groupNumber" +
+    /** diy 챌린지를 진행중인 사용자와 그 개수 */
+    @Query(value = "select p.memId as memId, count(p) as count from GroupPersonEntity p " +
+            "join GroupEntity g on p.groupNumber = g.groupNumber " +
             "where g.groupState=2 and g.groupClass = 2" +
-            "group by g.memId;")
-    List<Objects[]> findDiyCount();
+            "group by p.memId")
+    List<DiyCountDto> findDiyCount();
 
-    /** 만보기 결과 및 개인 목표 조회 */
-    @Query(value = "select p.pedometerResult, l.goalPedometer from PedometerEntity p "+
-            "join MemberEntity m on p.memId = m.memId" +
-            "join GodlifeGoalEntity l on l.memId = m.memId" +
-            "where p.pedometerDate = :date")
-    List<Objects[]> findPedometerResult(@Param("date")Date date);
+    /** 만보기 목표 달성한 사용자 조회 */
+    @Query(value = "select m.memId from PedometerEntity p "+
+            "join MemberEntity m on p.memId = m.memId " +
+            "join GodlifeGoalEntity l on l.memId = m.memId " +
+            "where p.pedometerDate=:date and p.pedometerResult>l.goalPedometer")
+    List<String> findPedometerResult(@Param("date")Date date);
+
+    /** diy 챌린지 달성 현황 */
+    @Query(value = "select count(c) from DiyCertifyEntity c where certifyDate=:date and memId=:memId")
+    Optional<Integer> findDiySuccess(@Param("date")Date date, @Param("memId")String memId);
 
 }
